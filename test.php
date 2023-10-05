f1d94086b61122cc54164f7c6a0c720c8a7201b5
JayAldrin <j-p0371@students.uc-bcf.edu.ph>
2023-10-05 18:29:59 +0800

    add test

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Aldrin";
+$age = 20;
+$greetings = print("Hi my name is $name and I am $age years old");
+
+
+print($greetings);
+
+
+
+function greetings() {
+    $names = "nov and khurt";
+    echo($names);
+}
+
+greetings();
+?>
